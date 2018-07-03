d077e0b44f0ec3500bbaced623ef3da9e586f05d
Tom Nunez <tomjnunez@gmail.com>
2018-07-03 09:34:21 -0600

    Creating a deck and deck test file

---
+++ test/deck_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/deck'
+
+class DeckTest < Minitest::Test
+  def test_it_exists
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    
+    assert_instance_of Deck, deck
+  end
+
+  def test_it_has_cards
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+
+    assert_equal [card_1, card_2, card_3], deck.cards
+  end
+
+  def test_it_counts
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+
+    assert_equal 3, deck.count
+  end
+end
